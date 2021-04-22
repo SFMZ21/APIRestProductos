f4d8a211084c537807e0427f2928dd1b26ef0e73
Sulma Martinez <smartinezz1@miumg.edu.gt>
2021-04-21 23:43:32 -0600

    Proyecto terminado
    API realizada con los videos

---
+++ src/test/java/com/umg/ProductosAPI/ProductosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.ProductosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
